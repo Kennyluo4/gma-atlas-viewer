2864a6c9a62ad8554df8ab29757eedc55da04026
ziliangluo <kennyluo4@gmail.com>
2025-01-09 17:32:24 -0500

    remove feedback option

--- streamlit_app.py
+++ streamlit_app.py
@@ -207,15 +207,15 @@ def main():
                 """
     st.markdown(footnote, unsafe_allow_html=True)
     
-    if sample_name == None:
-        sentiment_mapping = ["one", "two", "three", "four", "five"]
-        st.markdown('Rate our website')
-        selected = st.feedback('stars')
-        if selected is not None:
-            st.markdown(f'You selected {sentiment_mapping[selected]} star(s).')
-            record = str(selected) + '\n'
-            with open('feedbacks.txt', 'a') as handle:
-                handle.write(record)
+    # if sample_name == None:
+    #     sentiment_mapping = ["one", "two", "three", "four", "five"]
+    #     st.markdown('Rate our website')
+    #     selected = st.feedback('stars')
+    #     if selected is not None:
+    #         st.markdown(f'You selected {sentiment_mapping[selected]} star(s).')
+    #         record = str(selected) + '\n'
+    #         with open('feedbacks.txt', 'a') as handle:
+    #             handle.write(record)
                 
 if __name__ == '__main__':
     main()
